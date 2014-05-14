578bff9b9cb4e5cf1cf87279c1525955fc59e6e3
Pascal Gläser <pasi1995@gmail.com>
2014-05-14 11:32:45 +0200

    add calculation and output messages

--- src/com/ph3nx/java/bmi/BMI.java
+++ src/com/ph3nx/java/bmi/BMI.java
@@ -13,7 +13,8 @@ import javax.swing.JTextField;
 
 public class BMI extends JFrame implements ActionListener {
 	
-	JLabel l1, l2, l3;
+	private static final long serialVersionUID = 1L;
+	JLabel l1, l2, l3, l4;
 	JTextField t1, t2;
 	JButton b;
 	JCheckBox c1, c2;
@@ -26,10 +27,12 @@ public class BMI extends JFrame implements ActionListener {
 		setSize(300,300);
 		setLocation(100,100);
 		setLayout(new FlowLayout());
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		l1 = new JLabel("Größe");
 		l2 = new JLabel("Gewicht");
 		l3 = new JLabel("Dein BMI beträgt:");
+		l4 = new JLabel("");
 		
 		t1 = new JTextField(3);
 		t2 = new JTextField(3);
@@ -44,13 +47,33 @@ public class BMI extends JFrame implements ActionListener {
 		b.addActionListener(this);
 		add(l1); add(t1);
 		add(l2); add(t2);
-		add(c1); add(c2); add(b); add(l3);
+		add(c1); add(c2); add(b); add(l3); add(l3);
 		
 		setVisible(true);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		
+		String s1 = t1.getText();
+		String s2 = t2.getText();
+		
+		groesse = Double.parseDouble(s1);
+		gewicht = Double.parseDouble(s2);
+		
+		// status checkbox auswerten
+		if (c1.isSelected()) bmi = gewicht / (groesse*groesse);
+		if (c2.isSelected()) bmi = gewicht / (groesse*groesse) - 1;
+		
+		l3.setText("Dein BMI beträgt: " + bmi);
+		
+		if (bmi > 25) {
+			l4.setText("U got too much fat.");
+		} else if ( bmi < 20) {
+			l4.setText("U're skinny.");
+		} else {
+			l4.setText("You're alright.");
+		}
 		
 	}
 
